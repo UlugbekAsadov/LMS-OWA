db17b2d497c4ec287c31d7f69b7fc83b048e17c3
Ulugbek laptop <ulugb3k270@gmail.com>
2023-07-31 11:28:51 +0500

    feat: implemented react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import { Router } from "./router/router.jsx";
 
 const client = new QueryClient({
   defaultOptions: { queries: { staleTime: 5000 } },
@@ -11,7 +11,7 @@ const client = new QueryClient({
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
-      <App />
+      <Router />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>

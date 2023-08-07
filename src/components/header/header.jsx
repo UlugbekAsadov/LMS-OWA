c8478f23cec862a82a00511b4c55825860717756
shaxzod <shokirovshaxzod32@gmail.com>
2023-08-07 15:34:50 +0500

    lms-10 feat: added logo on dashbord and remove dropdown items

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,22 +1,23 @@
-import classNames from "classnames";
+import classNames from 'classnames';
+import { ReactComponent as BrandLogo } from '../../assets/icons/logo.svg';
 // import Logo from "../logo/Logo";
 // import User from "./dropdown/user/User";
 
-import Toggle from "../sidebar/toggle";
-import PropTypes from "prop-types";
-import { useTheme, useThemeUpdate } from "../../context";
-import { User } from "../user/user";
+import Toggle from '../sidebar/toggle';
+import PropTypes from 'prop-types';
+import { useTheme, useThemeUpdate } from '../../context';
+import { User } from '../user/user';
+import { Link } from 'react-router-dom';
 
 export const Header = ({ fixed, className }) => {
   const theme = useTheme();
   const themeUpdate = useThemeUpdate();
 
   const headerClass = classNames({
-    "nk-header": true,
-    "nk-header-fixed": fixed,
-    [`is-light`]: theme.header === "white",
-    [`is-${theme.header}`]:
-      theme.header !== "white" && theme.header !== "light",
+    'nk-header': true,
+    'nk-header-fixed': fixed,
+    [`is-light`]: theme.header === 'white',
+    [`is-${theme.header}`]: theme.header !== 'white' && theme.header !== 'light',
     [`${className}`]: className,
   });
 
@@ -31,7 +32,11 @@ export const Header = ({ fixed, className }) => {
               click={themeUpdate.sidebarVisibility}
             />
           </div>
-          <div className="nk-header-brand d-xl-none">Logo</div>
+          <div className="nk-header-brand d-xl-none">
+            <Link to={'/'}>
+              <BrandLogo />
+            </Link>
+          </div>
 
           <div className="nk-header-tools">
             <ul className="nk-quick-nav">
--- src/components/user/user.jsx
+++ src/components/user/user.jsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
-import { DropdownToggle, DropdownMenu, Dropdown } from "reactstrap";
-import { Link } from "react-router-dom";
-import { Icon } from "../icon/icon";
-import { LinkItem, LinkList } from "../links/links";
+import { useState } from 'react';
+import { DropdownToggle, DropdownMenu, Dropdown } from 'reactstrap';
+import { useNavigate } from 'react-router-dom';
+import { Icon } from '../icon/icon';
+import { LinkList } from '../links/links';
 
 export const User = () => {
   const [open, setOpen] = useState(false);
   const toggle = () => setOpen((prevState) => !prevState);
+  const navigate = useNavigate();
+
+  const handleLogOut = () => {
+    localStorage.removeItem('u_at');
+    navigate('/auth-login');
+  };
 
   return (
     <Dropdown isOpen={open} className="user-dropdown" toggle={toggle}>
@@ -20,57 +26,22 @@ export const User = () => {
       >
         <div className="user-toggle">
           <div className="user-avatar">
-            <span>AB</span>
+            <span>
+              <Icon name="user-alt"></Icon>
+            </span>
           </div>
           <div className="user-info d-none d-md-block">
-            <div className="user-status">Administrator</div>
             <div className="user-name dropdown-indicator">Abu Bin Ishityak</div>
           </div>
         </div>
       </DropdownToggle>
       <DropdownMenu end className="dropdown-menu-md dropdown-menu-s1">
-        <div className="dropdown-inner user-card-wrap bg-lighter d-none d-md-block">
-          <div className="user-card sm">
-            <div className="user-avatar">
-              <span>AB</span>
-            </div>
-            <div className="user-info">
-              <span className="lead-text">Abu Bin Ishtiyak</span>
-              <span className="sub-text">info@softnio.com</span>
-            </div>
-          </div>
-        </div>
         <div className="dropdown-inner">
           <LinkList>
-            <LinkItem
-              link="/user-profile-regular"
-              icon="user-alt"
-              onClick={toggle}
-            >
-              View Profile
-            </LinkItem>
-            <LinkItem
-              link="/user-profile-setting"
-              icon="setting-alt"
-              onClick={toggle}
-            >
-              Account Setting
-            </LinkItem>
-            <LinkItem
-              link="/user-profile-activity"
-              icon="activity-alt"
-              onClick={toggle}
-            >
-              Login Activity
-            </LinkItem>
-          </LinkList>
-        </div>
-        <div className="dropdown-inner">
-          <LinkList>
-            <Link href={`/auth-login`}>
+            <div className="cursor-pointer" onClick={handleLogOut}>
               <Icon name="signout"></Icon>
               <span>Sign Out</span>
-            </Link>
+            </div>
           </LinkList>
         </div>
       </DropdownMenu>

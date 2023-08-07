b36a16ab3763407c1061d714b0aa68f665e85616
shaxzod <shokirovshaxzod32@gmail.com>
2023-08-07 16:03:58 +0500

    LMS-10: bugfix: fixed sidbar active classes

--- src/utils/mocks/sidebar.mock.js
+++ src/utils/mocks/sidebar.mock.js
@@ -1,29 +1,30 @@
 export const menuMock = [
   {
-    icon: "file-docs",
-    text: "Shartnomalar",
+    icon: 'file-docs',
+    text: 'Shartnomalar',
+    link: '/contract',
     active: false,
     subMenu: [
       {
-        text: "Oddiy shartnomalar",
-        link: "/basic-contract",
+        text: 'Oddiy shartnomalar',
+        link: '/basic-contract',
       },
       {
-        text: "Kelajak kasblari",
-        link: "/grand-contract",
+        text: 'Kelajak kasblari',
+        link: '/grand-contract',
       },
     ],
   },
   {
-    icon: "todo",
-    text: "Kurslar",
-    link: "/courses-list",
+    icon: 'todo',
+    text: 'Kurslar',
+    link: '/courses-list',
     active: false,
   },
   {
-    icon: "panel",
-    text: "Shartnomalar turi",
-    link: "/contracts-type-list",
+    icon: 'panel',
+    text: 'Shartnomalar turi',
+    link: '/contracts-type-list',
     active: false,
   },
 ];

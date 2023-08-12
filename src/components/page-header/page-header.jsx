f288253866805e077d8f73e90d578067f60c5bf9
shaxzod <shokirovshaxzod32@gmail.com>
2023-08-12 13:38:54 +0500

    LMS-18 faet: add new prop

--- src/components/page-header/page-header.jsx
+++ src/components/page-header/page-header.jsx
@@ -1,37 +1,36 @@
-import { Button } from "reactstrap";
-import {
-  BlockBetween,
-  BlockDes,
-  BlockHead,
-  BlockHeadContent,
-  BlockTitle,
-} from "../blog/blog";
-import { Icon } from "../icon/icon";
+import {Button} from "reactstrap";
+import {BlockBetween, BlockDes, BlockHead, BlockHeadContent, BlockTitle,} from "../blog/blog";
+import {Icon} from "../icon/icon";
 import PropTypes from "prop-types";
 
 const PageHeader = ({
-  pageTitle,
-  pageDescription,
-  btnTitle,
-  btnIcon,
-  onClickButton = () => {},
-}) => {
-  return (
-    <BlockHead size="sm">
-      <BlockBetween>
-        <BlockHeadContent>
-          <BlockTitle>{pageTitle}</BlockTitle>
-          <BlockDes className="text-soft">{pageDescription}</BlockDes>
-        </BlockHeadContent>
-        <BlockHeadContent>
-          <Button color="primary" onClick={onClickButton}>
-            <Icon name={btnIcon}></Icon>
-            <span>{btnTitle}</span>
-          </Button>
-        </BlockHeadContent>
-      </BlockBetween>
-    </BlockHead>
-  );
+                        pageTitle,
+                        pageDescription,
+                        btnTitle,
+                        btnIcon,
+                        onClickButton = () => {
+                        },
+                        isButtonVisible,
+                    }) => {
+    return (
+        <BlockHead size="sm">
+            <BlockBetween>
+                <BlockHeadContent>
+                    <BlockTitle>{pageTitle}</BlockTitle>
+                    <BlockDes className="text-soft">{pageDescription}</BlockDes>
+                </BlockHeadContent>
+                {isButtonVisible && (
+                    <BlockHeadContent>
+                        <Button color={`primary`} onClick={onClickButton}>
+                            <Icon name={btnIcon}></Icon>
+                            <span>{btnTitle}</span>
+                        </Button>
+                    </BlockHeadContent>
+                )}
+
+            </BlockBetween>
+        </BlockHead>
+    );
 };
 
 export default PageHeader;
@@ -42,4 +41,5 @@ PageHeader.propTypes = {
   btnTitle: PropTypes.string,
   btnIcon: PropTypes.string,
   onClickButton: PropTypes.func,
+  isButtonVisible: PropTypes.bool
 };

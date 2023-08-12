13aa1f1ba58369ecf399103958f82cda19368406
Ulugbek <ulugbek4work@gmail.com>
2023-08-12 21:21:16 +0500

    route bug fix part 19

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import { Router } from './router/router.jsx';
 import { BasicContractsProvider } from './context/basic-contract.context.jsx';
 
 import './styles/scss/dashlite.scss';
@@ -19,7 +18,8 @@ import Login from './pages/auth/login.jsx';
 import Error403Classic from './pages/error/403-classic.jsx';
 import Error500Classic from './pages/error/500-classic.jsx';
 import Error404Classic from './pages/error/404-classic.jsx';
-
+import ThemeProvider from './context/theme.context.jsx';
+import { HomepageLayout } from './layout/homepage-layout/homepage-layout';
 const client = new QueryClient({
   defaultOptions: { queries: { staleTime: 5000 } },
 });
@@ -28,36 +28,40 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
       <BasicContractsProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Navigate to="/auth-login" />} />
+        <ThemeProvider>
+          <HomepageLayout>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Navigate to="/auth-login" />} />
 
-            <Route path="/basic-contracts" element={<BasicContracts />} />
-            <Route
-              path="/educational-center"
-              element={<EducationalCentersPage />}
-            />
-            <Route path="/staffs-list" element={<StaffsPage />} />
-            <Route
-              path="/grand-contract"
-              element={<FutureProfessionsContract />}
-            />
-            <Route path="/courses-list" element={<CoursesList />} />
-            <Route
-              path="/contracts-type-list"
-              element={<ContractsTypeList />}
-            />
-            <Route
-              path="/contracts-type-list/add-contract"
-              element={<CreateContract />}
-            />
+                <Route path="/basic-contracts" element={<BasicContracts />} />
+                <Route
+                  path="/educational-center"
+                  element={<EducationalCentersPage />}
+                />
+                <Route path="/staffs-list" element={<StaffsPage />} />
+                <Route
+                  path="/grand-contract"
+                  element={<FutureProfessionsContract />}
+                />
+                <Route path="/courses-list" element={<CoursesList />} />
+                <Route
+                  path="/contracts-type-list"
+                  element={<ContractsTypeList />}
+                />
+                <Route
+                  path="/contracts-type-list/add-contract"
+                  element={<CreateContract />}
+                />
 
-            <Route path="/auth-login" element={<Login />} />
-            <Route path="/no-access" element={<Error403Classic />} />
-            <Route path="/server-error" element={<Error500Classic />} />
-            <Route path="*" element={<Error404Classic />} />
-          </Routes>
-        </BrowserRouter>
+                <Route path="/auth-login" element={<Login />} />
+                <Route path="/no-access" element={<Error403Classic />} />
+                <Route path="/server-error" element={<Error500Classic />} />
+                <Route path="*" element={<Error404Classic />} />
+              </Routes>
+            </BrowserRouter>
+          </HomepageLayout>
+        </ThemeProvider>
         <ReactQueryDevtools initialIsOpen={false} />
       </BasicContractsProvider>
     </QueryClientProvider>

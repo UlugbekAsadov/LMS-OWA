deb34766aec8687951f9207688c99bc5f3cf6735
shaxzod <shokirovshaxzod32@gmail.com>
2023-08-14 16:31:02 +0500

    LMS-17 feat: table list connect to backend

--- src/pages/courses/courses-list.jsx
+++ src/pages/courses/courses-list.jsx
@@ -1,54 +1,64 @@
-import {Content} from "../../layout/page-layout/page-layout.jsx";
+import { Content } from "../../layout/page-layout/page-layout.jsx";
 import PageHeader from "../../components/page-header/page-header.jsx";
-import {Icon, Table} from "../../components/index.js";
-import {TablePagination} from "../../components/pagination/pagination.jsx";
-import {tableDataListMock} from "../../utils/mocks/index.js";
-import {useState} from "react";
-import {Link} from "react-router-dom";
-import {Button} from "reactstrap";
+import { Icon, Table } from "../../components/index.js";
+import { TablePagination } from "../../components/pagination/pagination.jsx";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "reactstrap";
+import { useQuery } from "react-query";
+import { getCoursesQuery } from "../../react-query/queries/index.js";
 
 const CoursesList = () => {
-    const [currentPage, setCurrentPage] = useState(1);
-    const [itemPerPage] = useState(20);
-    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemPerPage] = useState(20);
+  const { isLoading, data } = useQuery({
+    queryFn: () => getCoursesQuery(),
+    queryKey: ["all-courses-list"],
+  });
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-    const indexOfLastItem = currentPage * itemPerPage;
-    const indexOfFirstItem = indexOfLastItem - itemPerPage;
-    const currentItems = tableDataListMock.slice(indexOfFirstItem, indexOfLastItem);
+  const indexOfLastItem = currentPage * itemPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemPerPage;
+  const currentItems = data?.slice(indexOfFirstItem, indexOfLastItem);
 
 
-    const tableHeader = (
-        <thead className="tb-odr-head">
-        <tr className="tb-odr-item">
-            <th className="tb-odr-info">
-                <span className="tb-odr-id">Raqami</span>
-                <span className="tb-odr-date d-none d-md-inline-block">FIO</span>
-            </th>
-            <th className="tb-odr-amount">
-                <span className="tb-odr-total">Tel raqami</span>
-                <span className="tb-odr-status d-none d-md-inline-block">Kursi</span>
-            </th>
-            <th className="tb-odr-action">&nbsp;</th>
-        </tr>
-        </thead>
-    );
-    const tableBody = currentItems.map((item, index) => {
-        return (
-            <tr className="tb-odr-item" key={item.id}>
-                <td className="tb-odr-info">
+  const tableHeader = (
+    <thead className="tb-odr-head">
+    <tr className="tb-odr-item">
+      <th className="tb-odr-info">
+        <span className="tb-odr-id">Raqami</span>
+        <span className="tb-odr-date d-none d-md-inline-block">Nomi</span>
+      </th>
+      <th className="tb-odr-amount">
+        <span className="tb-odr-total">Narxi</span>
+        <span className="tb-odr-status d-none d-md-inline-block">Muddati</span>
+      </th>
+      <th className="tb-odr-action">&nbsp;</th>
+    </tr>
+    </thead>
+  );
+
+  if(isLoading){
+    return
+  }
+
+  const tableBody = currentItems.map((item, index) => {
+    return (
+      <tr className="tb-odr-item" key={item.id}>
+        <td className="tb-odr-info">
                     <span className="tb-odr-id">
                       <Link to={`/invoice-details/${item.id}`}>
                         {currentPage * 20 + index + 1 - 20}
                       </Link>
                     </span>
-                    <span className="tb-odr-date">{item.name}</span>
-                </td>
-                <td className="tb-odr-amount">
+          <span className="tb-odr-date">{item.name}</span>
+        </td>
+        <td className="tb-odr-amount">
                     <span className="tb-odr-total">
-                      <span className="amount">{item.phone}</span>
+                      <span className="amount">{item.price} so'm</span>
                     </span>
-                    <span className="tb-odr-total">
-                          {item.date}
+          <span className="tb-odr-total">
+                          {item.month} oy
                     </span>
                 </td>
                 <td className="tb-odr-action">
@@ -79,18 +89,19 @@ const CoursesList = () => {
                 }}
             />
 
-            <Table
-                pagination={
-                    <TablePagination
-                        itemPerPage={itemPerPage}
-                        totalItems={tableDataListMock.length}
-                        paginate={paginate}
-                        currentPage={currentPage}
-                    />
-                }
-                tableBody={currentItems.length ? tableBody : null}
-                tableHeader={tableHeader}
-            />
+          <Table
+            pagination={
+              <TablePagination
+                itemPerPage={itemPerPage}
+                totalItems={data.length}
+                paginate={paginate}
+                currentPage={currentPage}
+              />
+            }
+            tableBody={currentItems.length ? tableBody : null}
+            isLoading={isLoading}
+            tableHeader={tableHeader}
+          />
 
         </Content>
     )

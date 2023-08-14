6d0ddebf35345e3270aec230e71d1a5881323734
shaxzod <shokirovshaxzod32@gmail.com>
2023-08-14 14:30:10 +0500

    LMS-23 feat: implemented validation on add staff modal

--- src/components/modals/modal-staffe/add-staff.jsx
+++ src/components/modals/modal-staffe/add-staff.jsx
@@ -1,35 +1,28 @@
 import {Col, Label, Modal, ModalBody} from "reactstrap";
 import PropTypes from "prop-types";
 import {Icon} from "../../icon/icon.jsx";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import {useForm} from "react-hook-form";
 import RSelect from "../../react-select/react-select.jsx";
 import Button from "../../button/button.jsx";
+import {rolesMock} from "../../../utils/mocks";
+import {InputMask} from "primereact/inputmask";
 
 
 const AddStaff = ({isOpen, onClose}) => {
-    const {register, handleSubmit, formState: {errors}} = useForm();
-    const [cities, setCities] = useState([]);
-    const [selectedCity, setSelectedCity] = useState(null);
+    const {register, handleSubmit, setValue, formState: {errors}} = useForm();
+
+    const [selectedRole, setSelectedRole] = useState(null);
     const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
-    const mockDataRol = [
-        { value: "blues", label: "Blues" },
-        { value: "rock", label: "Rock" },
-        { value: "jazz", label: "Jazz" },
-        { value: "orchestra", label: "Orchestra" },
-    ]
-
-    useEffect(() => {
-        setCities(mockDataRol)
-    }, [])
-
     const handleChangeCity = (value) => {
-        console.log(value.value)
-        setSelectedCity(value);
+        setValue('rol', value.value)
+        setSelectedRole(value);
     };
 
     const handleSubmitForm = (values) => {
+        const regex = /\((\d{2})\) (\d{3})-(\d{2})-(\d{2})/;
+        values.phone_number = values.phone_number.replace(regex, '$1$2$3$4');
         console.log({values, errors});
     };
     return (
@@ -46,33 +39,42 @@ const AddStaff = ({isOpen, onClose}) => {
                         </p>
                     </div>
                     <div className="form-group">
-                        <Label htmlFor="default-0" className="form-label fs-6">
+                        <Label htmlFor="name_surname" className="form-label fs-6">
                             Ismi va familiyasi
                         </Label>
                         <div className="form-control-wrap">
                             <input
-                                className="form-control form-control-lg"
+                                className={`form-control form-control-lg ${errors.first_name && "error"}`}
                                 type="text"
-                                id="default-0"
-                                {...register("first_name")}
+                                id="name_surname"
+                                {...register("first_name", {required: 'Ismi yoki familiyani kiriting'})}
                             />
+                            {errors.first_name && (
+                                <span className="invalid">{errors.first_name.message}</span>
+                            )}
                         </div>
                     </div>
                     <div className="form-group">
-                        <Label htmlFor="default-0" className="form-label fs-6">
+                        <Label htmlFor="phone_number" className="form-label fs-6">
                             Telefon raqami
                         </Label>
                         <div className="form-control-wrap">
-                            <input
-                                className="form-control form-control-lg"
-                                type="text"
-                                id="default-0"
-                                {...register("first_number")}
+                            <InputMask
+                                id="phone_number"
+                                {...register('phone_number', {
+                                    required: 'Telefon raqamni kiriting',
+                                })}
+                                className={`form-control-lg form-control  ${errors.first_name && "error"}`}
+                                mask="(99) 999-99-99"
+                                placeholder="(99) 999-99-99"
                             />
+                            {errors.phone_number && (
+                                <span className="invalid">{errors.phone_number.message}</span>
+                            )}
                         </div>
                     </div>
                     <div className="form-group">
-                        <Label htmlFor="default-0" className="form-label fs-6">
+                        <Label htmlFor="password" className="form-label fs-6">
                             Parol
                         </Label>
                         <div className="form-control-wrap">
@@ -94,22 +96,22 @@ const AddStaff = ({isOpen, onClose}) => {
                                 type={isPasswordVisible ? "text" : "password"}
                                 id="password"
                                 {...register("password", {
-                                    required: "This field is required",
+                                    required: "Parolni kiriting",
                                 })}
-                                className={`form-control-lg form-control  ${
+                                className={`form-control-lg form-control ${errors.first_name && "error"} ${
                                     isPasswordVisible ? "is-hidden" : "is-shown"
                                 }`}
                             />
-                            {errors.passcode && (
-                                <span className="invalid">{errors.passcode.message}</span>
+                            {errors.password && (
+                                <span className="invalid">{errors.password.message}</span>
                             )}
                         </div>
                     </div>
                     <div className="form-group  ">
                         <label className="form-label">Rol</label>
                         <RSelect
-                            options={cities}
-                            value={selectedCity}
+                            options={rolesMock}
+                            value={selectedRole}
                             onChange={handleChangeCity}
                         />
                     </div>

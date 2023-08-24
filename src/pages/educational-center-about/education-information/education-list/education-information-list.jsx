cd90b39cd8925b7a66a57f7e57602b8d529ac063
shaxzod <shokirovshaxozd32@gmail.com>
2023-08-24 14:48:56 +0500

    LMS-33 feat: fix date and phone add modal

---
+++ src/pages/educational-center-about/education-information/education-list/education-information-list.jsx
@@ -0,0 +1,126 @@
+import { Row } from "reactstrap";
+import { useQuery } from "react-query";
+import { getEducationalInformationQueryFn } from "../../../../react-query/queries/educational.query.js";
+import { Loader } from "../../../../components/index.js";
+
+const EducationInformationList = () => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["educational-information"],
+    queryFn: () => getEducationalInformationQueryFn(),
+  });
+  const parseDate = (date) => {
+    let dateObject = new Date(date);
+    let months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    return `
+    ${dateObject.getUTCDate()} ${months[dateObject.getUTCMonth()]},
+     ${dateObject.getUTCFullYear()} ${dateObject.getUTCHours()}:${dateObject.getUTCMinutes()} ${
+       dateObject.getUTCHours() >= 12 ? "PM" : "AM"
+     }`;
+  };
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  return (
+    <Row className={"py-3 gy-5"}>
+      <table className="table table-lg preview-reference">
+        <thead className="table-light"></thead>
+        <tbody>
+          <tr>
+            <td>Oq’uv markazi nomi (brend)</td>
+            <td>{data.name_brand}</td>
+          </tr>
+          <tr>
+            <td>Tel raqami</td>
+            <td>
+              <p>
+                {data.phone.replace(
+                  /^(\d{2})(\d{3})(\d{2})(\d{2})$/,
+                  "+998 $1 $2 $3 $4"
+                )}
+              </p>
+            </td>
+          </tr>
+          <tr>
+            <td>Yuridik nomi</td>
+            <td>
+              <p>{data.name_legal}</p>
+            </td>
+          </tr>
+          <tr>
+            <td>Direktor ismi qisqa</td>
+            <td>
+              <p>{data.director_name_short}</p>
+            </td>
+          </tr>
+          <tr>
+            <td>Viloyat</td>
+            <td>
+              <p>{data.region.name_lt}</p>
+            </td>
+          </tr>
+          <tr>
+            <td>Shaxar/Tuman</td>
+            <td>
+              <p>{data.district.name_lt}</p>
+            </td>
+          </tr>
+          <tr>
+            <td>Qo’shimcha ma’lumot</td>
+            <td>
+              <p>{parseDate(data.createdAt)}</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      {/*Bank info*/}
+
+      <div>
+        <p className={"text-soft fs-4"}>Bank info</p>
+        <table className="table table-lg preview-reference">
+          <thead className="table-light"></thead>
+          <tbody>
+            <tr>
+              <td>Bank kodi</td>
+              <td>{data.bank_code}</td>
+            </tr>
+            <tr>
+              <td>Bank</td>
+              <td>
+                <p>{data.bank_name}</p>
+              </td>
+            </tr>
+            <tr>
+              <td>INN</td>
+              <td>
+                <p>{data.inn}</p>
+              </td>
+            </tr>
+            <tr>
+              <td>Hisob raqami</td>
+              <td>
+                <p className={""}>{data.bank_account}</p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </Row>
+  );
+};
+export default EducationInformationList;
--- src/pages/educational-center-about/education-information/educational-information.jsx
+++ src/pages/educational-center-about/education-information/educational-information.jsx
@@ -1,105 +1,27 @@
 import { Content } from "../../../layout/page-layout/page-layout.jsx";
 import PageHeader from "../../../components/page-header/page-header.jsx";
-import { Row } from "reactstrap";
-import { useQuery } from "react-query";
-import { getEducationalInformationQueryFn } from "../../../react-query/queries/educational.query.js";
-import { Loader } from "../../../components/index.js";
+import EducationInformationList from "./education-list/education-information-list.jsx";
+import { useState } from "react";
+import AddBootcampsModal from "../../../components/modals/add-bootcamps-modal/add-bootcamps-modal.jsx";
 
 const EducationalInformation = () => {
-  const { data, isLoading } = useQuery({
-    queryKey: ["educational-information"],
-    queryFn: () => getEducationalInformationQueryFn(),
-  });
-  if (isLoading) {
-    return <Loader />;
-  }
+  const [isModalOpen, setIsOpenModal] = useState(false);
   return (
     <Content title="Ma’lumot">
       <PageHeader
         pageTitle={"Ma’lumotlar"}
         btnTitle={"O’zgartirish"}
         isButtonVisible
+        onClickButton={setIsOpenModal.bind(null, true)}
       />
-      <Row className={"py-3 gy-5"}>
-        <table className="table table-lg preview-reference">
-          <thead className="table-light"></thead>
-          <tbody>
-            <tr>
-              <td>Oq’uv markazi nomi (brend)</td>
-              <td>{data.name_brand}</td>
-            </tr>
-            <tr>
-              <td>Tel raqami</td>
-              <td>
-                <p>+{data.phone}</p>
-              </td>
-            </tr>
-            <tr>
-              <td>Yuridik nomi</td>
-              <td>
-                <p>{data.name_legal}</p>
-              </td>
-            </tr>
-            <tr>
-              <td>Direktor ismi qisqa</td>
-              <td>
-                <p>{data.director_name_short}</p>
-              </td>
-            </tr>
-            <tr>
-              <td>Viloyat</td>
-              <td>
-                <p>{data.region.name_lt}</p>
-              </td>
-            </tr>
-            <tr>
-              <td>Shaxar/Tuman</td>
-              <td>
-                <p>{data.district.name_lt}</p>
-              </td>
-            </tr>
-            <tr>
-              <td>Qo’shimcha ma’lumot</td>
-              <td>
-                <p>{data.createdAt}</p>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <EducationInformationList />
 
-        {/*Bank info*/}
-
-        <div>
-          <p className={"text-soft fs-4"}>Bank info</p>
-          <table className="table table-lg preview-reference">
-            <thead className="table-light"></thead>
-            <tbody>
-              <tr>
-                <td>Bank kodi</td>
-                <td>{data.bank_code}</td>
-              </tr>
-              <tr>
-                <td>Bank</td>
-                <td>
-                  <p>{data.bank_name}</p>
-                </td>
-              </tr>
-              <tr>
-                <td>INN</td>
-                <td>
-                  <p>{data.inn}</p>
-                </td>
-              </tr>
-              <tr>
-                <td>Hisob raqami</td>
-                <td>
-                  <p>{data.bank_account}</p>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </Row>
+      {isModalOpen && (
+        <AddBootcampsModal
+          isOpen={isModalOpen}
+          onClose={setIsOpenModal.bind(null, false)}
+        />
+      )}
     </Content>
   );
 };

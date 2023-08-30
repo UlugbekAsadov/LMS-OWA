f2cdb57cd18418b6e25a871a637ecba7d093f5b0
Ulugbek laptop <ulugb3k270@gmail.com>
2023-08-31 00:23:21 +0500

    feat: redirection added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import ThemeProvider from "./context/theme.context";
 import { HomepageLayout } from "./layout/homepage-layout/homepage-layout";
 import "./styles/css/global.css";
+import { useEffect } from "react";
+import { menuMock } from "./utils/mocks/index.js";
+import { useQuery } from "react-query";
 
 function App() {
+  const navigate = useNavigate();
+  const { data: userData } = useQuery({
+    queryKey: ["user"],
+  });
+
+  const firstAccessibleLink = menuMock.filter((menu) =>
+    menu.access.find((role) => role === userData.role)
+  );
+
+  useEffect(() => {
+    navigate(firstAccessibleLink[0].link);
+  }, [firstAccessibleLink, navigate]);
+
   return (
     <ThemeProvider>
       <HomepageLayout>

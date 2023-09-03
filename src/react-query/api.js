eab0664cb9dd6e144bf2cafcb02d6583189ee626
Ulugbek laptop <ulugb3k270@gmail.com>
2023-09-03 16:18:05 +0500

    feat: some changes

--- src/react-query/api.js
+++ src/react-query/api.js
@@ -11,7 +11,7 @@ export const api = async (url, config) => {
 
   // eslint-disable-next-line no-undef
   if (process.env.NODE_ENV === "production") {
-    apiUrl = `${window.location.hostname}/api${url}`;
+    apiUrl = `/api${url}`;
   }
 
   try {
